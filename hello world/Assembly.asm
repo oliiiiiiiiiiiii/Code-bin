c0c8f801efddeb1e9fcd4dff3df6f0d4169b4840
Oliiiii <74825334+oliiiiiiiiiiiii@users.noreply.github.com>
2021-09-14 20:19:45 +0530

    Update Assembly.asm

--- hello world/Assembly.asm
+++ hello world/Assembly.asm
@@ -1,15 +1,15 @@
 section    .text
-    global _start       ;must be declared for using gcc
-_start:                     ;tell linker entry point
-    mov    edx, len    ;message length
-    mov    ecx, msg    ;message to write
-    mov    ebx, 1        ;file descriptor (stdout)
-    mov    eax, 4        ;system call number (sys_write)
-    int    0x80        ;call kernel
-    mov    eax, 1        ;system call number (sys_exit)
-    int    0x80        ;call kernel
+    global _start       ;
+_start:                     ;
+    mov    edx, len    ;
+    mov    ecx, msg    ;
+    mov    ebx, 1        ;
+    mov    eax, 4        ;
+    int    0x80        ;
+    mov    eax, 1        ;
+    int    0x80        ;
 
 section    .data
 
-msg    db    'Hello, world!',0xa    ;our dear string
-len    equ    $ - msg            ;length of our dear string
+msg    db    'Hello, world!',0xa    ;
+len    equ    $ - msg            ;
