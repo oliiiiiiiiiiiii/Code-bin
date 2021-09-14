dae5c8b5a90a7cbe1bc76de3d0b1b9e4c032d91d
Oliiiii <74825334+oliiiiiiiiiiiii@users.noreply.github.com>
2021-09-14 21:11:21 +0530

    Update guess_the_number.py

--- python/guess_the_number.py
+++ python/guess_the_number.py
@@ -1,15 +1,14 @@
-from random import randint
+from random import randint #importing randint function from random library
 
-max_number = input("Enter the max number you want:")
+max_number = int(input("Enter the max number you want:")) #enter the max limit of the randomly generated number and make it an integer
 
-ans = True
+ans = True 
 
-while ans:
-  number = randint(1,max_number)
-  guess = input("Guess the number: ")
-  ans = number == guess
-  if ans:
-    print("Wow you guessed it right")
+while ans: #since we defined ans as True so it will keep iterating unless ans becomes False
+  number = randint(1,max_number) #the randint function generates a random number such that : if randint(a,b) , a <= number <= b
+  guess = int(input("Guess the number: ")) #to take input from user about thier guess and converting it into an integer
+  if number==guess: #checking equality of the guess and the randomly genrated number
+    print("Wow you guessed it right") 
   else:
-    print("You tried unfortunately it was not right better luck next time.")
-    break
+    print("You tried unfortunately it was incorrect better luck next time.")
+    break #exiting from the loop if the guess is incorrect
