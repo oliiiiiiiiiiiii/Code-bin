e9b74f376809ddc18de136b24f4f7d92cb454fd7
Oliiiii <74825334+oliiiiiiiiiiiii@users.noreply.github.com>
2021-09-14 20:42:46 +0530

    Create search_google.py

---
+++ python/search_google.py
@@ -0,0 +1,8 @@
+from webbrowser import open #we only need the open() function so we will just import the open function from the std python lib
+
+query = input("Enter Your Query: ") #here enter what your query is
+
+open(f"https://www.google.com/search?q={query}") #lets now search our query on google
+
+'''You will need python version 3.6 or above because f strings were introduced in python 3.6,
+If you want to do the program in a earlier version you will need'''
